b383eb88a82ef8fee3e68e49db6a667352726560
fernando nunez <fernunezmendoza@gmail.com>
2026-09-05 22:52:32 +0200

    feat(backend): add tracing to redis from otpl

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.0
 require (
 	github.com/golang-jwt/jwt/v4 v4.5.2
 	github.com/jackc/pgx/v5 v5.10.0
+	github.com/redis/go-redis/extra/redisotel/v9 v9.21.0
 	github.com/redis/go-redis/v9 v9.21.0
 	go.mongodb.org/mongo-driver v1.17.9
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.44.0
@@ -17,6 +18,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/redis/go-redis/extra/rediscmd/v9 v9.21.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.44.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.10.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
--- shared/db/redisdb.go
+++ shared/db/redisdb.go
@@ -1,14 +1,17 @@
 package db
 
 import (
+	"log"
+
 	"domino/shared/env"
 
+	"github.com/redis/go-redis/extra/redisotel/v9"
 	redis "github.com/redis/go-redis/v9"
 )
 
 type RedisConfig struct {
 	URI string
-	//Database string
+	// Database string
 }
 
 // Reads REDISDB_URI from the environment
@@ -19,9 +22,13 @@ func NewRedisDefaultConfig() *RedisConfig {
 	}
 }
 
-// NewRedisClient creates a new Redis client with the given config
+// NewRedisClient creates a new Redis client with the given config.
+// Every command issued on the returned client is automatically instrumented with otlp span
 func NewRedisClient(config *RedisConfig) *redis.Client {
 	opt, _ := redis.ParseURL(config.URI)
 	client := redis.NewClient(opt)
+	if err := redisotel.InstrumentTracing(client); err != nil {
+		log.Printf("redis: failed to instrument tracing: %v", err)
+	}
 	return client
 }
